40451b518a943cb0a32ff3b6bb898c0ccdda67db
beateullmann <beate.ullmann@stud.fh-luebeck.de>
2017-01-14 23:09:31 +0100

    Changed the routes, added necassary imports (short: alt+Enter)

--- app/app-routing.module.ts
+++ app/app-routing.module.ts
@@ -7,15 +7,19 @@ import { UsersComponent} from './users/users.component';
 import { QuestionsComponent} from './questions/questions.component';
 import { CategoriesComponent} from './categories/categories.component';
 import { RegisterUserComponent} from './users/register-user.component';
+import { AuthGuard } from "./guards/auth.guard";
+import {LoginUserComponent} from "./users/login-user.component";
+import {WelcomeComponent} from "./welcome/welcome.component";
 
 
 //Routenplaner - Bsp: Wenn Pfad /users aufgerufen wird, dann includiere die UsersComponent (unter router-outlet im DOM)
 const routes: Routes = [
-  {path: '', redirectTo: '/categories', pathMatch:'full'},
+  {path: '', component: WelcomeComponent, canActivate:[AuthGuard] },
   {path: 'users', component: UsersComponent},
   {path: 'questions', component: QuestionsComponent},
   {path: 'categories', component: CategoriesComponent},
-  {path: 'register', component: RegisterUserComponent}
+  {path: 'register', component: RegisterUserComponent},
+  {path: 'login', component: LoginUserComponent}
 ]
 
 @NgModule({
