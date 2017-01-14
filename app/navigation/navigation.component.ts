e67f099f096bf150bbf37f47b342dbc90d7de880
beateullmann <beate.ullmann@stud.fh-luebeck.de>
2017-01-14 22:53:08 +0100

    Navigation ts component created

---
+++ app/navigation/navigation.component.ts
@@ -0,0 +1,13 @@
+/**
+ * Created by beateullmann on 14.01.17.
+ */
+import { Component} from '@angular/core';
+
+
+@Component({
+  moduleId: module.id,
+  selector: 'bbq-nav',
+  templateUrl: 'navigation.component.html'
+})
+export class NavigationComponent {
+}
